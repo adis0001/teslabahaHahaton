7fc3f38c7b612e66caef09cfa9100da126a59be0
Adis <ulanbekadis552@gmail.com>
2022-02-04 16:54:03 +0600

    fake

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MyNavbar from "../Navbar/MyNavbar";
+import "../Home/home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <MyNavbar />
+      <div className="homePhoto"></div>
+    </div>
+  );
+};
+
+export default Home;
